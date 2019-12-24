704eb5ac63954c01374f296aef4ca30a0d39fa83
Panagiotis Karelis <karelispanagiotis@gmail.com>
2019-12-24 22:15:58 +0200

    Changed CudaV1 implementation. Now grid size matches lattice

--- src/ising_cudaV1.cpp
+++ src/ising_cudaV1.cpp
@@ -7,8 +7,6 @@
 
 #define epsilon 1e-6f
 
-#define BLK_SZ 512
-
 __device__ __forceinline__ 
 int mod(int x, int y)
 {
@@ -16,19 +14,16 @@ int mod(int x, int y)
 }
 
 __device__  __forceinline__ 
-int calcLatticePos(int pos, int n, int xOffset, int yOffset)
+int calcLatticePos(int i, int j, int n, int xOffset, int yOffset)
 {
     /* Finds the index in the lattice, according to
-     *  pos: Current Position (in which we calculate spin)
+     *  i, j   : Current Position (in which we calculate spin)
      *  xOffset: Offset in the x-axis
      *  yOffset: Offset in the y-axis
-     * -----------------------------------------------------
-     *  pos/n : is the row that corresponds to pos
-     *  pos%n : is the column that corresponds to pos
      */
 
     // Perform Modular Arithmetic
-    return mod(pos/n + yOffset, n)*n + mod(pos%n + xOffset, n);  
+    return mod(i + yOffset, n)*n + mod(j + xOffset, n);  
 }
 
 void swapPtr(int** ptr1, int** ptr2)
@@ -40,25 +35,20 @@ void swapPtr(int** ptr1, int** ptr2)
 
 __global__ void calculateSpin(int *current, int *next, float *w, int n)
 {
-    size_t index = (size_t)blockIdx.x * BLK_SZ + threadIdx.x;
+    size_t index = (size_t)blockIdx.y*n + blockIdx.x;
+
+    float result = 0.0f;
+    int i,j;
+    for(i=-2; i<=2; i++)
+        for(j=-2; j<=2; j++)
+            result += w[ (i+2)*5 + (j+2) ] * current[ calcLatticePos(blockIdx.y, blockIdx.x, n, j, i) ];
     
-    if(index < (size_t)n*n)
-    {
-        float result = 0.0f;
-        int i,j;
-        for(i=-2; i<=2; i++)
-            for(j=-2; j<=2; j++)
-                result += w[ (i+2)*5 + (j+2) ] * current[ calcLatticePos(index, n, j, i) ];
-        
-        if(fabsf(result) < epsilon )
-            next[index] = current[index];
-        else if(result < 0)
-            next[index] = -1;
-        else
-            next[index] = 1;
-    }
-    
-    
+    if(fabsf(result) < epsilon )
+        next[index] = current[index];
+    else if(result < 0)
+        next[index] = -1;
+    else
+        next[index] = 1;
 }
 
 void ising(int *G, float *w, int k, int n)
@@ -76,9 +66,10 @@ void ising(int *G, float *w, int k, int n)
     cudaMemcpy(dev_w, w, 25*sizeof(float), cudaMemcpyHostToDevice);
 
     // Kernel Launch - Calculations
+    dim3 grid2D(n, n);
     for(int iter=0; iter<k; iter++)
     {
-        calculateSpin<<<((size_t)n*n + BLK_SZ - 1)/BLK_SZ, BLK_SZ>>>(dev_G, dev_G2, dev_w, n);
+        calculateSpin<<<grid2D, 1>>>(dev_G, dev_G2, dev_w, n);
         cudaDeviceSynchronize();
         swapPtr(&dev_G, &dev_G2);
     }
